4a52ddf8b12b56165a5e38f753c97caa1146e3a6
nancymahmoud1 <nancymahmoud2003@gmail.com>
2025-03-12 22:57:12 +0200

    task 2 ui

--- app/controller.py
+++ app/controller.py
@@ -1,7 +1,8 @@
 from PyQt5 import QtWidgets
 from app.ui.design import Ui_MainWindow
-# from design import Ui_MainWindow
 from app.utils.clean_cache import remove_directories
+
+
 class MainWindowController:
     def __init__(self):
         self.app = QtWidgets.QApplication([])
@@ -9,13 +10,16 @@ class MainWindowController:
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self.MainWindow)
 
+        # Hide sidebar_2 and sidebar_3 initially
+        self.ui.sidebar_2_layout.hide()
+        self.ui.sidebar_3_layout.hide()
+        self.ui.parametric_shape_combobox.addItems(["Line","Circle", "Ellipse" ])
+
+        # Connect button signals
         self.ui.quit_app_button.clicked.connect(self.closeApp)
-
-        # Connect Back Button to show side_bar_1
-        self.ui.back_button.clicked.connect(self.show_sidebar_1)
-
-        # Connect the Edge Detection button to toggle_sidebar
-        self.ui.edge_detection_button.clicked.connect(self.toggle_sidebar)
+        self.ui.back_button.clicked.connect(self.show_sidebar_1)  # Connect back button
+        self.ui.edge_detection_button.clicked.connect(self.show_sidebar_2)
+        self.ui.object_contour_button.clicked.connect(self.show_sidebar_3)
 
     def run(self):
         """Run the application."""
@@ -23,23 +27,29 @@ class MainWindowController:
         self.app.exec_()
 
     def quit_app(self):
+        """Quit the application."""
         self.app.quit()
         remove_directories()
-
-    def toggle_sidebar(self):
-        """Show or hide Sidebar 2 when the Edge Detection button is clicked."""
-        if self.ui.sidebar_2.isVisible():
-            self.ui.sidebar_2.hide()
-        else:
-            self.ui.sidebar_2.show()
-
-    def show_sidebar_1(self):
-        """Show side_bar_1 when the back button is clicked."""
-        self.ui.sidebar_1_widget.show()  # Show the parent widget
-        self.ui.sidebar_2.hide()
 
     def closeApp(self):
         """Close the application."""
         remove_directories()
         self.app.quit()
 
+    def show_sidebar_1(self):
+        """Show sidebar_1 and hide other sidebars."""
+        self.ui.sidebar_2_layout.hide()  # Hide sidebar_2
+        self.ui.sidebar_3_layout.hide()  # Hide sidebar_3
+        self.ui.sidebar_1_layout.show()  # Show sidebar_1
+
+    def show_sidebar_2(self):
+        """Show sidebar_2 and hide other sidebars."""
+        self.ui.sidebar_1_layout.hide()  # Hide sidebar_1
+        self.ui.sidebar_3_layout.hide()  # Hide sidebar_3
+        self.ui.sidebar_2_layout.show()  # Show sidebar_2
+
+    def show_sidebar_3(self):
+        """Show sidebar_3 and hide other sidebars."""
+        self.ui.sidebar_1_layout.hide()  # Hide sidebar_1
+        self.ui.sidebar_2_layout.hide()  # Hide sidebar_2
+        self.ui.sidebar_3_layout.show()  # Show sidebar_3
