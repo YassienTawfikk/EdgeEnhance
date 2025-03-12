48a72802a3c6b9f2407491f518179875a009fc27
madonna-mosaad <madonnamosaad86@gmail.com>
2025-03-12 23:55:06 +0200

    image services

--- app/controller.py
+++ app/controller.py
@@ -1,6 +1,8 @@
 from PyQt5 import QtWidgets
 from app.ui.design import Ui_MainWindow
 from app.utils.clean_cache import remove_directories
+from app.services.image_service import ImageServices
+import cv2
 
 
 class MainWindowController:
@@ -20,6 +22,29 @@ class MainWindowController:
         self.ui.back_button.clicked.connect(self.show_sidebar_1)  # Connect back button
         self.ui.edge_detection_button.clicked.connect(self.show_sidebar_2)
         self.ui.object_contour_button.clicked.connect(self.show_sidebar_3)
+        self.srv = ImageServices()
+        self.ui.upload_button.clicked.connect(self.drawImage)
+        self.ui.save_button.clicked.connect(lambda: self.srv.save_image(self.processed_image))
+        self.ui.reset_button.clicked.connect(self.reset_images)
+
+    def drawImage(self):
+        self.path = self.srv.upload_image_file()
+        self.original_image = cv2.imread(self.path)
+        self.processed_image = self.original_image
+
+        # If user cancels file selection, path could be None
+        if self.path:
+            self.srv.clear_image(self.ui.original_image_groupbox)
+            self.srv.clear_image(self.ui.processed_image_groupbox)
+            self.srv.set_image_in_groupbox(self.ui.original_image_groupbox, self.original_image)
+            self.srv.set_image_in_groupbox(self.ui.processed_image_groupbox, self.processed_image)
+
+    def reset_images(self):
+        if self.original_image is None:
+            return
+
+        self.srv.clear_image(self.ui.processed_image_groupbox)
+        self.srv.set_image_in_groupbox(self.ui.processed_image_groupbox, self.original_image)
 
     def run(self):
         """Run the application."""
